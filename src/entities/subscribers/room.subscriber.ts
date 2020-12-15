b87db0330daed716df0b1ae6701f037f59ad72e2
tuankha17490 <tuankha1749@gmail.com>
2020-12-15 17:16:10 +0700

    fix delete room

--- src/entities/subscribers/room.subscriber.ts
+++ src/entities/subscribers/room.subscriber.ts
@@ -4,17 +4,16 @@ import { Room } from '../room.entity';
 
 @EventSubscriber()
 export class RoomSubscriber implements EntitySubscriberInterface<Room> {
-
   /**
-     * Indicates that this subscriber only listen to Post events.
-     */
+   * Indicates that this subscriber only listen to Post events.
+   */
   listenTo(): any {
     return Room;
   }
 
   /**
-     * Called before post insertion.
-     */
+   * Called before post insertion.
+   */
   async afterInsert(event: InsertEvent<Room>): Promise<void> {
     const { property } = event.entity;
     if (property.maxPrice < event.entity.price) property.maxPrice = event.entity.price;
@@ -26,28 +25,42 @@ export class RoomSubscriber implements EntitySubscriberInterface<Room> {
     await getConnection()
       .createQueryBuilder()
       .update(Property)
-      .set({ averageArea: property.averageArea, averagePrice: property.averagePrice, minPrice: property.minPrice, maxPrice: property.maxPrice })
+      .set({
+        averageArea: property.averageArea,
+        averagePrice: property.averagePrice,
+        minPrice: property.minPrice,
+        maxPrice: property.maxPrice
+      })
       .where('id = :id', { id: property.id })
       .execute();
   }
 
   async afterUpdate(event: InsertEvent<Room>): Promise<void> {
-    const { property } = event.entity;
-    if (property.maxPrice < event.entity.price) property.maxPrice = event.entity.price;
-    if (property.minPrice > event.entity.price) property.minPrice = event.entity.price;
-    const count = property.rooms.length;
-    property.rooms.forEach(room => {
-      if(room.id === event.entity.id) {
-        property.averagePrice = (property.averagePrice * count + event.entity.price - room.price) / count;
-        property.averageArea = (property.averageArea * count + event.entity.area - room.area) / count;
-      }
-    });
+    if (event.entity.property) {
+      const { property } = event.entity;
+      if (property.maxPrice < event.entity.price) property.maxPrice = event.entity.price;
+      if (property.minPrice > event.entity.price) property.minPrice = event.entity.price;
+      const count = property.rooms.length;
+      property.rooms.forEach(room => {
+        if (room.id === event.entity.id) {
+          property.averagePrice =
+            (property.averagePrice * count + event.entity.price - room.price) / count;
+          property.averageArea =
+            (property.averageArea * count + event.entity.area - room.area) / count;
+        }
+      });
 
-    await getConnection()
-      .createQueryBuilder()
-      .update(Property)
-      .set({ averageArea: property.averageArea, averagePrice: property.averagePrice, minPrice: property.minPrice, maxPrice: property.maxPrice })
-      .where('id = :id', { id: property.id })
-      .execute();
+      await getConnection()
+        .createQueryBuilder()
+        .update(Property)
+        .set({
+          averageArea: property.averageArea,
+          averagePrice: property.averagePrice,
+          minPrice: property.minPrice,
+          maxPrice: property.maxPrice
+        })
+        .where('id = :id', { id: property.id })
+        .execute();
+    }
   }
 }
